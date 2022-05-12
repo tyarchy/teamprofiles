8d2768d8428cc4ea649ee61b6bf220b550f16fed
tyarchy <ty.d.gregory@gmail.com>
2022-05-12 10:12:07 -0600

    html styling

--- src/generate-profile.js
+++ src/generate-profile.js
@@ -1,5 +1,22 @@
 const generateProfile = (data) =>{
-    return `<div><p>${data.name}<p></div>
+    return `
+    
+    
+    <div class="container">
+        <div class="card-deck">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${data.role}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${data.role}</h6>
+                    <p class="card-text">${data.id}</p>
+                    <a href="#" class="card-link">${data.email}</a>
+                    <a href="#" class="card-link">${data.github}</a>
+                </div>
+            </div>
+        </div>
+    </div> 
+                 
+    <div><p>${data.name}<p></div>
     `
 };
 
