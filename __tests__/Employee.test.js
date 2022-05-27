cbae645dc2c5a69252ae0dbe3ed6f4916ffbe709
tyarchy <ty.d.gregory@gmail.com>
2022-05-26 22:41:55 -0600

    I quit

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,16 +3,34 @@ const Employee = require('../lib/Employee');
 
 
 test('employee mega object', () => {
-  const employee = new Engineer('BillyBob', 1, 'email');
+  const employee = new Employee('BillyBob', 1, 'email');
   
   expect(employee.name) .toEqual(expect.any(String));
+  expect(employee.id) .toEqual(expect.any(String));
+  expect(employee.email) .toEqual(expect.any(String));
+  
+});
+
+test('get name', () =>{
+  const employee = new Employee('BillyBob', 1, 'email', );
+  expect(employee.name) .toEqual(expect.any(String));
+});
+
+test('get id', () =>{
+  const employee = new Employee('BillyBob', 1, 'email', );
+  expect(employee.id) .toEqual(expect.any(Number));
+});
+
+test('get id', () =>{
+  const employee = new Employee('BillyBob', 1, 'email', );
+  expect(employee.email) .toEqual(expect.any(String));
 });
 
 
 
-// gets role from getRole() 
+// gets role () 
 test('gets role of employee', () => {
-  const employee = new Employee('BillyBob', 90, 'email', 'github');
+  const employee = new Employee('BillyBob', 1, 'email', );
 
-  expect(employee.getRole()).toEqual("Engineer");
+  expect(employee.getRole()).toEqual("roleSpecific");
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -9,16 +9,16 @@ test('creates an Engineer object', () => {
     expect(engineer.github) .toEqual(expect.any(String));
 });
 
-// gets github from getGithub()
+// gets github
 test('gets engineer github value', () => {
     const engineer = new Engineer('Bob', 1, 'email', 'github');
 
     expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
 });
 
-// gets role from getRole() 
+// gets role 
 test('gets role of employee', () => {
-    const engineer = new Engineer('Bob', 90, 'email', 'github');
+    const engineer = new Engineer('Bob', 1, 'email', 'github');
 
     expect(engineer.getRole()).toEqual("Engineer");
 });
